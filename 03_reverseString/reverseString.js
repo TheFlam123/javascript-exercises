0c95355bbc2c16d7da8a381abb0c1244ed33e7ed
Kaustubh <kalulkar@gmail.com>
2023-05-28 23:20:56 +1000

    Complete function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    prompt();
+    let stringSplit = str.split("");
+    let reverseArr = stringSplit.reverse();
+    let joinArr = reverseArr.join("");
+    return joinArr;
 };
 
 // Do not edit below this line
